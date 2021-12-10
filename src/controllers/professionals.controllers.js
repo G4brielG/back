dbefea823fe524f7dcf22760c7de6a48aab7a377
G4brielG <gabriel240901@gmail.com>
2021-12-10 19:55:43 -0300

    a

--- src/controllers/professionals.controllers.js
+++ src/controllers/professionals.controllers.js
@@ -3,7 +3,10 @@ const Profesionales = require("../models/professional.models");
 const controller = {};
 
 controller.getProfesionales = async (_req, res) => {
-  const profesionales = await Profesionales.find({ es_profesional: true });
+  const profesionales = await Profesionales.find({
+    es_profesional: true,
+    active: true,
+  });
 
   res.json(profesionales);
 };
